cc28a2e755e3f1a6c05a2bcfaa69851f03b5873f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-12-06 10:43:59 -0300

    feat: React Server Components

--- src/app/catalog/product/[...product]/page.tsx
+++ src/app/catalog/product/[...product]/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 interface ProductProps {
   params: {
     product: string[]
@@ -7,11 +8,17 @@ interface ProductProps {
 export default function Product({ params }: ProductProps) {
   const [id, size, color] = params.product
 
+  function handleAddToCar() {
+    const test = { 'Adicionado ao carrinho': 'test' }
+    console.log(test['Adicionado ao carrinho'])
+  }
+
   return (
     <div>
       <p>Product: {id}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+      <button onClick={handleAddToCar}>Adicionar ao carrinho</button>
     </div>
   )
 }
